21b0e290aed22853ff8499240ed24bb19d50ae55
Nicolas Ferreira <nickrfer@gmail.com>
2018-06-19 23:58:52 -0300

    Reverse str and palindrome

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -9,8 +9,13 @@
 
 function palindrome(str) {
   let reversed = str.split('').reduce((rev, char) => char + rev, '');
-
   return str === reversed;
+}
+
+function palindrome2(str) {
+  return str.split('').every((char, i) => {
+    return char === str[str.length -i -1];
+  });
 }
 
 module.exports = palindrome;
